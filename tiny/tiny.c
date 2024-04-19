8ed7ec70da3e5997cea18e6cf09462da5dc273ec
J-Jaeh <1wo2gur@naver.com>
2024-04-19 12:48:37 +0000

    [feat] implement clienterror()

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -133,3 +133,31 @@ int isAuthority(struct stat sbuf)
     return 1;
   return 0;
 }
+
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg)
+{
+  char buf[MAXLINE];
+  char body[MAXLINE];
+
+  /*HTTP response body*/
+  sprintf(body, "<html><title> Tiny Error</title>");
+  sprintf(body, "%s<body bgcolor="
+                "ffffff"
+                ">\r\n",
+          body);
+  sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+  sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
+  sprintf(body, "%s<hr><em> The Tiny Web server</em?\r\n", body);
+
+  /*print the HTTP response*/
+  sprintf(buf, "HTTP/1.0 %s %s \r\n", errnum, shortmsg);
+  Rio_writen(fd, buf, strlen(buf));
+
+  sprintf(buf, "Content-type : text/html\r\n");
+  Rio_writen(fd, buf, strlen(buf));
+
+  sprinft(buf, "Content-lenght : %d \r\n\r\n", (int)strlen(body));
+  Rio_writen(fd, buf, strlen(buf));
+
+  Rio_writen(fd, body, strlen(body));
+}
