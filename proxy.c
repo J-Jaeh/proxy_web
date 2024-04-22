b24bf80cafd46f15c425ebc4b85f940c74be8af4
J-Jaeh <1wo2gur@naver.com>
2024-04-22 08:55:16 +0000

    [feat] save

--- proxy.c
+++ proxy.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "csapp.h"
 
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
@@ -9,7 +12,163 @@ static const char *user_agent_hdr =
     "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
     "Firefox/10.0.3\r\n";
 
-int main() {
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
+
+int parse_uri(const char *request, char *server_ip, char *server_port, char *request_uri);
+void proxy_to_server(int server_fd);
+
+int main(int argc, char **argv)
+{
   printf("%s", user_agent_hdr);
-  return 0;
+
+  int listenfd, connfd, serverfd;
+  char hostname[MAXLINE], port[MAXLINE];
+  socklen_t clientlen;
+  struct sockaddr_storage clientaddr;
+
+  rio_t rio;
+
+  // char serverIP[MAXLINE] = "13.125.79.202";
+  // char serverPort[MAXLINE] = "4000";
+
+  if (argc != 2)
+  {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    exit(1);
+  }
+
+  listenfd = Open_listenfd(argv[1]);
+  while (1)
+  {
+    clientlen = sizeof(clientaddr);
+    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+    Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
+    printf("Accepted connection from (%s, %s) & proxy port : %s\n", hostname, port, argv[1]);
+
+    // serverfd = Open_clientfd(serverIP, serverPort);
+    proxy_to_server(connfd);
+
+    Close(connfd);
+  }
+}
+
+void proxy_to_server(int client_fd)
+{
+  int server_fd;
+  rio_t rio;
+
+  char client_buf[MAXLINE];
+  char client_method[MAXLINE];
+  char client_version[MAXLINE];
+  char request[MAXLINE];
+
+  char server_IP[MAXLINE];
+  char server_port[MAXLINE];
+  char request_uri[MAXLINE];
+
+  char request_buf[MAXLINE];
+  char response_buf[MAXLINE];
+
+  Rio_readinitb(&rio, client_fd);
+  Rio_readlineb(&rio, client_buf, MAXLINE);
+  sscanf(client_buf, "%s %s %s", client_method, request, client_version);
+  // printf("client_buf %s\n", client_buf);
+  // printf("request %s\n", request);
+
+  if (!parse_uri(request, server_IP, request_uri, server_port))
+  {
+    printf("Failed to parse URI\n");
+    return;
+  }
+
+  // printf("%s %s %s\n", server_IP, server_port, request_uri);
+
+  server_fd = Open_clientfd(server_IP, server_port);
+
+  snprintf(request_buf, MAXLINE, "%s %s HTTP/1.0\r\n:", client_method, request_uri);
+  Rio_writen(server_fd, request_buf, strlen(request_buf));
+  while (strcmp(request_buf, "\r\n"))
+  {
+    Rio_readlineb(&rio, request_buf, MAXLINE);
+    Rio_writen(server_fd, request_buf, strlen(request_buf));
+  }
+  // 일단 여기가지 서버요청에 요청 쌉가능 이제 서버가 보내준걸 받기만하면됨!
+
+  // 서버로부터 응답을 받아 클라이언트로 전달
+  Rio_readinitb(&rio, server_fd);
+  while (Rio_readlineb(&rio, response_buf, MAXLINE) > 0)
+  {
+    Rio_writen(client_fd, response_buf, strlen(response_buf));
+  }
+
+  Close(server_fd);
+}
+
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg)
+{
+  char buf[MAXLINE];
+  char body[MAXLINE];
+
+  /*HTTP response body*/
+  sprintf(body, "<html><head><link rel=\"shortcut icon\" href=\"#\"><title>Tiny Error</title></head></html>");
+  sprintf(body, "%s<body bgcolor="
+                "ffffff"
+                ">\r\n",
+          body);
+  sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+  sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
+  sprintf(body, "%s<hr><em> The Tiny Web server</em>\r\n", body);
+
+  /*print the HTTP response*/
+  sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+  Rio_writen(fd, buf, strlen(buf));
+
+  sprintf(buf, "Content-type : text/html\r\n");
+  Rio_writen(fd, buf, strlen(buf));
+
+  sprintf(buf, "Content-length : %d \r\n\r\n", (int)strlen(body));
+  Rio_writen(fd, buf, strlen(buf));
+
+  Rio_writen(fd, body, strlen(body));
+}
+
+int parse_uri(const char *request, char *server_ip, char *path, char *server_port)
+{
+  char *temp = strdup(request);
+
+  // 첫 '/' 제거
+  char *uri = temp + 1;
+
+  // http:// 또는 https://를 찾는다.
+  if (strncmp(uri, "http://", 7) == 0)
+  {
+    strcpy(server_port, "80");
+    uri += 7; // "http://" 이후의 문자열로 이동
+  }
+  else if (strncmp(uri, "https://", 8) == 0)
+  {
+    strcpy(server_port, "443");
+    uri += 8; // "https://" 이후의 문자열로 이동
+  }
+
+  // '/'를 기준으로 서버 주소와 경로를 분리한다.
+  char *slash = strchr(uri, '/');
+  if (slash != NULL)
+  {
+    *slash = '\0';           // 서버 주소와 경로를 분리하기 위해 '/'를 NULL로 변경
+    strcpy(path, slash + 1); // 경로 복사
+  }
+
+  // ':'를 찾아 IP와 포트 번호를 분리한다.
+  char *colon = strchr(uri, ':');
+  if (colon != NULL)
+  {
+    *colon = '\0';                  // IP와 포트 번호를 분리하기 위해 ':'를 NULL로 변경
+    strcpy(server_port, colon + 1); // 포트 번호 복사
+  }
+
+  strcpy(server_ip, uri); // 서버 IP 복사
+
+  free(temp);
+  return 1; // 파싱 성공
 }
--- tiny/tiny.c
+++ tiny/tiny.c
@@ -63,8 +63,12 @@ int main(int argc, char **argv)
     clientlen = sizeof(clientaddr);
     connfd = Accept(listenfd, (SA *)&clientaddr,
                     &clientlen); // line:netp:tiny:accept
-    Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE,
-                0);
+    // Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
+    if (getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0) != 0)
+    {
+      fprintf(stderr, "Getnameinfo error: %s\n", strerror(errno));
+      // exit(1);
+    }
     printf("Accepted connection from (%s, %s) & server port : %s\n", hostname, port, argv[1]);
     doit(connfd);  // line:netp:tiny:doit -> HTTP 트랜잭션처리
     Close(connfd); // line:netp:tiny:close
@@ -92,6 +96,12 @@ void doit(int fd)
   printf("Request headers:\n");
   printf("%s", buf);
   sscanf(buf, "%s %s %s", method, uri, version);
+  // favicon 무시
+  if (strstr(uri, "favicon"))
+  {
+    printf("Tiny couldn't find the favicon.");
+    return;
+  }
 
   if ((strcasecmp(method, "GET")) && strcasecmp(method, "HEAD"))
   {
@@ -309,7 +319,7 @@ void serve_dynamic(int fd, char *filename, char *cgiargs, int head_flag)
    */
   sleep(5);
 
-  // if (Fork() == 0) // Fork 하면 자식 pid 리턴한다 ~
+  if (Fork() == 0) // Fork 하면 자식 pid 리턴한다 ~
   {
     /* Child*/
     /* Real server would set all CGI vars here */
@@ -329,49 +339,6 @@ void serve_dynamic(int fd, char *filename, char *cgiargs, int head_flag)
      * emptylist : 실행파일에 전달되는 argument를 지정... 현재는 아무것도 전달안함
      * eniron : ? 새로운 환경변수 목록일 지정.. 새로 실행되는 프로그램이 현재 프로세스와 동일한 환경 설정을 상속받도록하기 위해 ..?
      */
-    // Execve(filename, emptylist, environ);
-    adder();
+    Execve(filename, emptylist, environ);
   }
-}
-
-void adder()
-{
-  char *buf; /*쿼리스트링 문자를 담을 버퍼*/
-  char *p;
-  char arg1[MAXLINE];
-  char arg2[MAXLINE];
-  char content[MAXLINE]; /* HTTP response body*/
-
-  int n1 = 0, n2 = 0;
-
-  /* Extract the two arguments */
-  if ((buf = getenv("QUERY_STRING")) != NULL) /* serve_dynamic 함수에서 set_env에서 지정한 key값? 으로 가져옴 약간 map 구조인듯?*/
-  {
-    p = strchr(buf, '&');  /* strchr-> pram 1 에 있는 문자열에서 pram2를 찾는 함수 */
-    *p = '\0';             /* 위에서 찾은 & 문자를 NULL 처리 -> 문자열의 끝 */
-    strcpy(arg1, buf + 2); /* buf 값을 arg1에 복사*/
-    strcpy(arg2, p + 3);   /* 두번째 인자값을 arg2에 복사(p는 &위치를 가리키고있었으니까+1)*/
-    n1 = atoi(arg1);       /* atoi 는 문자열을 숫자로 변경하는것*/
-    n2 = atoi(arg2);
-  }
-
-  /* Make the response body */
-  /* sprintf 는 문자열을 형식에 맞게 생성하는 함수 , content에 저장*/
-
-  sprintf(content, "%sWelcom to add.com: ", content);
-  sprintf(content, "%sTHE Internet addition portal. \r\n<p>", content);
-  sprintf(content, "%sThe answer is : %d + %d = %d\r\n<p>", content, n1, n2, n1 + n2);
-  sprintf(content, "%sThanks for visiting!\r\n", content);
-
-  /* Generate the HTTP response */
-  /* CGI의경우 stdout의 버퍼를 사용자의 웹브라우저로 보내는 역할을!*/
-  /* 그런데 CGI를 이용해 웹브라우저로 보낼때는 상태라인과 헤더를 포함해야함 ~ .상태는 어디 포함되는거 ? 함수 호출전에 처리한듯 */
-  printf("Connection : close\r\n");
-  printf("Content-length: %d\r\n", (int)strlen(content));
-  printf("Content-type:  text/html\r\n\r\n"); /* \r\n\r\n 헤더와 본문사이를 구분하기위한 약속*/
-  char *str = getenv("REQUEST_METHOD");
-  // printf("pointer : %s", str);
-  if (!strcasecmp(str, "GET"))
-    printf("%s", content);
-  fflush(stdout); /* stdout 버퍼 비우기*/
 }
