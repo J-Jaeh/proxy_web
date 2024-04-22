23fde00914639ebd39b73028621ac4e1487c583c
J-Jaeh <1wo2gur@naver.com>
2024-04-22 17:13:23 +0000

    [feat] 동시성 프로그래밍 - 프로세스

--- echo/echoserver.c
+++ echo/echoserver.c
@@ -2,30 +2,38 @@
 
 void echo(int);
 
+void sigchld_handler(int sig)
+{
+    while (waitpid(-1, 0, WNOHANG) > 0)
+        ;
+    return;
+}
+
 int main(int argc, char **argv)
 {
     int listendfd, connfd;
-    socklen_t clientlen;
-    struct sockaddr_storage clientaddr;
-    char client_hostname[MAXLINE], client_port[MAXLINE];
+    socklen_t clientlen = sizeof(struct sockaddr_in);
+    struct sockaddr_in clientaddr;
 
     if (argc != 2)
     {
-        fprintf(stderr, "usage : %s <port>\n", argv[0]);
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
         exit(0);
     }
-
-    listendfd = Open_listenfd(argv[1]);
+    Signal(SIGCHLD, sigchld_handler);
+    listendfd = Open_listenfd(atoi(argv[1]););
     while (1)
     {
-        clientlen = sizeof(struct sockaddr_storage);
         connfd = Accept(listendfd, (SA *)&clientaddr, &clientlen);
-        Getnameinfo((SA *)&clientaddr, clientlen, client_hostname, MAXLINE, client_port, MAXLINE, 0);
-        printf("Connected to (%s %s)\n", client_hostname, client_port);
-        echo(connfd);
+        if (Fork() == 0)
+        {
+            Close(listendfd); // -> 자식은 듣기 소켓 꺼도됨 !?
+            echo(connfd);
+            Close(connfd);
+            exit(0);
+        }
         Close(connfd);
     }
-    exit(0);
 }
 void echo(int connfd)
 {
